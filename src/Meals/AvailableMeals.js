db580ac95d91ad64eda8eb19d79ba9f631f12f8e
roshinys <roshinchaya@gmail.com>
2022-12-10 16:27:03 +0530

    added mealItem component

--- src/Meals/AvailableMeals.js
+++ src/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
@@ -29,8 +31,15 @@ const DUMMY_MEALS = [
 ];
 
 function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
-  return <section className={classes.meals}>{mealsList}</section>;
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return <Card className={classes.meals}>{mealsList}</Card>;
 }
 
 export default AvailableMeals;
